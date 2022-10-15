954207875093423c5b3aad5f4e26426cdd8838a3
kenta-python <kenta.python@gmail.com>
2022-10-16 07:34:33 +0900

    lacked_number

---
+++ ABC/ABC248/lacked_number.py
@@ -0,0 +1,18 @@
+s = input()
+
+sum = 0
+for i in range(len(s)):
+  sum += int(s[i])
+
+print(45 - sum)
+
+
+'''
+def solve():
+  s = input()
+  for i in range(10):
+    if str(i) not in s:
+      return i
+
+print(solve())
+'''
