cdfc4e03fc55e721401914b36d074f923804771a
kenta-python <kenta.python@gmail.com>
2022-10-15 20:32:45 +0900

    jogging

---
+++ ABC/ABC249/jogging.py
@@ -0,0 +1,16 @@
+def solve(a, b, c, x):
+  q = x // (a + c)
+  r = x % (a + c)
+  return (q * a + min(a, r))*b
+
+a, b, c, d, e, f, x = map(int, input().split())
+
+takahashi = solve(a, b, c, x)
+aoki = solve(d, e, f, x)
+
+if takahashi > aoki:
+  print('Takahashi')
+elif aoki > takahashi:
+  print('Aoki')
+else:
+  print('Draw')
