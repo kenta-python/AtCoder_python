3b669d04391a3ba20e654a8bed82f62c429a3a3a
kenta-python <kenta.python@gmail.com>
2022-10-16 17:31:52 +0900

    maintain_multiple_sequence

---
+++ ABC/ABC271/B_maintain_multiple_sequence.py
@@ -0,0 +1,12 @@
+n, q = map(int, input().split())
+a = [[] for _ in range(n)]
+for i in range(n):
+  a[i] = list(map(int, input().split()))
+for j in range(q):
+  s, t = map(int, input().split())
+  print(a[s-1][t])
+
+
+
+  
+
