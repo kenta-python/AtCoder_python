093f1cd8a393586f6fe0904e8c15f98d26d5be32
kenta-python <kenta.python@gmail.com>
2022-10-16 09:38:28 +0900

    exponential_or_quadratic

---
+++ ABC/ABC238/exponential_or_quadratic.py
@@ -0,0 +1,16 @@
+# pythonだとTLE,pypyだとAC
+
+n = int(input())
+
+if 2**n > n**2:
+  print('Yes')
+else:
+  print('No')
+
+'''
+n = int(input())
+if 2 <= n <= 4:
+  print('No')
+else:
+  print('Yes')
+'''
