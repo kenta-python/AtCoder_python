5c3b11e7b16b5d5351fe65986a3daa20be1bb51e
kenta-python <kenta.python@gmail.com>
2022-11-16 21:27:20 +0900

    yamanote line game

---
+++ ABC/ABC244/C_yamanote_line_game.py
@@ -0,0 +1,11 @@
+def main():
+  n = int(input())
+  rem = set(range(1, (2*n)+2))
+  while True:
+    print(rem.pop(), flush=True)
+    a = int(input())
+    if a == 0: break
+    rem.discard(a)
+
+if __name__ == '__main__':
+  main()
