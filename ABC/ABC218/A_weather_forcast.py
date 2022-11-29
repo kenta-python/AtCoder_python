9d4f5f947230623b676eac1a5d51b7769032d2ae
kenta-python <kenta.python@gmail.com>
2022-11-29 14:35:48 +0900

    ABC218

---
+++ ABC/ABC218/A_weather_forcast.py
@@ -0,0 +1,7 @@
+n = int(input())
+s = list(input())
+
+if s[n-1] == 'o':
+  print('Yes')
+else:
+  print('No')
---
+++ ABC/ABC218/B_qwerty.py
@@ -0,0 +1,8 @@
+abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+p = list(map(int, input().split()))
+ans = []
+for i in p:
+  ans.append(abc[i-1])
+
+print(*ans, sep='')
