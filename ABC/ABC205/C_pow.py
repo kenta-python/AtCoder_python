41e01e4a741da98eddc306b567b06f1b803f1015
kenta-python <kenta.python@gmail.com>
2022-12-13 20:43:17 +0900

    pow

---
+++ ABC/ABC205/C_pow.py
@@ -0,0 +1,18 @@
+a, b, c = map(int, input().split())
+abs_a = abs(a)
+abs_b = abs(b)
+if c%2 == 0:
+  if abs_a > abs_b:
+    print('>')
+  elif abs_a == abs_b:
+    print('=')
+  else:
+    print('<')
+else:
+  if a > b :
+    print('>')
+  elif a == b:
+    print('=')
+  else:
+    print('<')
+  
